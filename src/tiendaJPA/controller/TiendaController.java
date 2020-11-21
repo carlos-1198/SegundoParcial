832037636a3d404c09da3f15245d72deed9d0df8
Carlos <carlosalbertoml@ufps.edu.co>
2020-11-21 08:22:22 -0500

    first commit

---
+++ src/tiendaJPA/controller/TiendaController.java
@@ -0,0 +1,13 @@
+package tiendaJPA.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/TiendaController")
+public class TiendaController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	public TiendaController() {
+		super();
+	}
+}
