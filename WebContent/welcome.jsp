5117ede3d20584d3c628f8735141f5933930cdb8
Carlos <carlosalbertoml@ufps.edu.co>
2020-11-21 09:42:46 -0500

    listarTiendas

---
+++ WebContent/welcome.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Nuestra tienda virtual</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="css/card.css">
+</head>
+<body>
+
+<!-- Team -->
+<section id="team" class="pb-5">
+    <div class="container">
+        <h5 class="section-title h1">SERVICIOS</h5>
+        <div class="row  justify-content-center pb-5">
+  
+            <div class="col-xs-12 col-sm-6 col-md-3">
+            	<a href="login.html" class="btn btn-lg btn-primary btn-block text-uppercase font-weight-bold mb-2 text-light" type="submit">Login</a>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3 align-self-center">
+            	<a href="registro.html" class="btn btn-lg btn-primary btn-block text-uppercase font-weight-bold mb-2 text-light" type="submit">Registrate</a>
+            </div>
+            
+            <div class="col-xs-12 col-sm-6 col-md-3 align-self-center">
+            	<a href="TiendaController?action=listar" class="btn btn-lg btn-primary btn-block text-uppercase font-weight-bold mb-2 text-light" type="submit">Index</a>
+            </div>
+        </div>
+      </div>
+</section>
+</body>
+</html>
--- src/tiendaJPA/controller/TiendaController.java
+++ src/tiendaJPA/controller/TiendaController.java
@@ -1,13 +1,87 @@
 package tiendaJPA.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tiendaJPA.dao.TiendaDao;
+import tiendaJPA.model.Tienda;
+
+
 
 @WebServlet("/TiendaController")
 public class TiendaController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
+	TiendaDao dao = new TiendaDao();
 	public TiendaController() {
 		super();
 	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String action = request.getParameter("action");
+		switch(action) {
+			case "index":
+				index(request, response);
+				break;
+			case "listar":
+				listarTiendas(request, response);
+				break;
+				/*
+			case "buscar":
+				buscarHeroe(request, response);
+				break;
+			case "insert":
+				registrarHeroe(request, response);
+				break;
+			case "nuevo":
+				showNewForm(request, response);
+				break;
+			case "listar":
+				listarHeroes(request, response);
+				break;
+			case "edit":
+				editarHeroe(request, response);
+				break;
+			case "update":
+				updateHeroe(request, response);
+				break;
+			case "delete":
+				deleteHeroe(request, response);
+				break;
+			default:
+				index(request, response);
+				break;
+			*/
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+	
+	private void index (HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException{
+		request.getRequestDispatcher("welcome.jsp").forward(request, response);
+	}
+	
+	public void listarTiendas(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		List <Tienda> listaTiendas = dao.list();
+		request.getSession().setAttribute("listTiendas", listaTiendas);
+		
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		
+	}
+	
 }
