4f9432f3c0339471e2c30d6add56cfd55950c5d8
Carlos <carlosalbertoml@ufps.edu.co>
2020-11-21 10:16:28 -0500

    registro tienda

--- src/tiendaJPA/controller/TiendaController.java
+++ src/tiendaJPA/controller/TiendaController.java
@@ -3,6 +3,7 @@ package tiendaJPA.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,13 +35,17 @@ public class TiendaController extends HttpServlet{
 			case "listar":
 				listarTiendas(request, response);
 				break;
+			case "nuevo":
+				showNewForm(request, response);
+				break;
+			case "insert":
+				registrarTienda(request, response);
+				break;
 				/*
 			case "buscar":
 				buscarHeroe(request, response);
 				break;
-			case "insert":
-				registrarHeroe(request, response);
-				break;
+			
 			case "nuevo":
 				showNewForm(request, response);
 				break;
@@ -82,6 +87,37 @@ public class TiendaController extends HttpServlet{
 		
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 		
+	}
+	
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+		    throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("registro.jsp");
+        dispatcher.forward(request, response);
+	}
+	
+	public void registrarTienda(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+
+		//if (esValido(request.getParameter("nombre")) && esValido(request.getParameter("alias"))
+				//&& esValido(request.getParameter("estado")) && esValido(request.getParameter("genero"))) {
+		Tienda tienda = new Tienda();
+		tienda.setClave(request.getParameter("InputPassword"));
+		tienda.setDescripcion(request.getParameter("inputDescripcion"));
+		tienda.setEmail(request.getParameter("inputEmail "));
+		tienda.setFacebook(request.getParameter("inputFacebook "));
+		tienda.setImagen("inputImagen");
+		tienda.setLema(request.getParameter("inputLema"));
+		tienda.setNombre(request.getParameter("inputNombre"));
+		tienda.setPropietario(request.getParameter("inputPropietario "));
+		tienda.setWeb(request.getParameter("inputWeb "));
+		
+
+		dao.insert(tienda);
+		
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		//} else {
+			//request.getRequestDispatcher("index.jsp").forward(request, response);
+		//}
 	}
 	
 }
